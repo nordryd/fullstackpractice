11a6ce8bc62b9f0f6f481e0923fdac498bac5bfa
Jacob Overton <32399688+nordryd@users.noreply.github.com>
2020-05-21 10:34:39 -0500

    Imported starter project from start.spring.io

---
+++ src/main/java/com/nordryd/fulleststack/FulleststackApplication.java
@@ -0,0 +1,13 @@
+package com.nordryd.fulleststack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulleststackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulleststackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nordryd/fulleststack/FulleststackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nordryd.fulleststack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FulleststackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
